db89239140833559761ae221c5c3de0e87d8470a
florian <150048765+El-Rata-Alada@users.noreply.github.com>
2026-02-07 20:48:53 +0530

    Improve technology and CMS detection in fprint.py
    Enhanced detection of web technologies and CMS by adding more headers and HTML checks.

--- theeye/modules/fprint.py
+++ theeye/modules/fprint.py
@@ -1,3 +1,6 @@
+import re
+
+
 def _check_requests():
     try:
         import requests
@@ -22,7 +25,10 @@ def main(url: str) -> dict:
     result = {
         "web_server": None,
         "technologies": [],
-        "cms": None
+        "cms": None,
+        "frameworks": [],
+        "javascript": [],
+        "cloud": None
     }
 
     if not url or not isinstance(url, str):
@@ -39,41 +45,88 @@ def main(url: str) -> dict:
     try:
         response = requests.get(url, timeout=10)
     except requests.exceptions.RequestException:
-        print("[!] No internet connection")
         return result
 
     headers = response.headers
 
-    # -------- Header-based detection --------
-    server = headers.get("Server")
-    if server:
-        result["web_server"] = server
+    # ---------------- HEADER-BASED DETECTION ----------------
 
-    powered = headers.get("X-Powered-By")
-    if powered:
-        result["technologies"].append(powered)
+    if headers.get("Server"):
+        result["web_server"] = headers.get("Server")
 
-    generator = headers.get("X-Generator")
-    if generator:
-        result["technologies"].append(generator)
+    for header in ["X-Powered-By", "X-Generator"]:
+        if headers.get(header):
+            result["technologies"].append(headers.get(header))
 
-    # -------- HTML-based detection --------
+    # Framework hints
+    framework_headers = {
+        "X-AspNet-Version": "ASP.NET",
+        "X-Django-Version": "Django",
+        "X-Runtime": "Ruby on Rails",
+        "X-Powered-By-Plesk": "Plesk"
+    }
+
+    for h, name in framework_headers.items():
+        if headers.get(h):
+            result["frameworks"].append(name)
+
+    # Cloud / CDN detection
+    if headers.get("CF-RAY"):
+        result["cloud"] = "Cloudflare"
+    elif headers.get("X-Amz-Cf-Id"):
+        result["cloud"] = "AWS CloudFront"
+    elif headers.get("X-Azure-Ref"):
+        result["cloud"] = "Azure"
+
+    # ---------------- HTML-BASED DETECTION ----------------
+
     try:
         soup = BeautifulSoup(response.text, "html.parser")
     except Exception:
         return result
 
+    # Meta generator
     meta_gen = soup.find("meta", attrs={"name": "generator"})
     if meta_gen and meta_gen.get("content"):
         gen = meta_gen["content"]
         result["technologies"].append(gen)
 
-        g = gen.lower()
-        if "wordpress" in g:
+        gl = gen.lower()
+        if "wordpress" in gl:
             result["cms"] = "WordPress"
-        elif "joomla" in g:
+        elif "joomla" in gl:
             result["cms"] = "Joomla"
-        elif "drupal" in g:
+        elif "drupal" in gl:
             result["cms"] = "Drupal"
+
+    # CMS fingerprints
+    html = response.text.lower()
+
+    if "/wp-content/" in html or "/wp-includes/" in html:
+        result["cms"] = "WordPress"
+
+    if "/sites/default/" in html:
+        result["cms"] = "Drupal"
+
+    if "content=\"joomla" in html:
+        result["cms"] = "Joomla"
+
+    # JavaScript libraries
+    scripts = soup.find_all("script", src=True)
+    for s in scripts:
+        src = s["src"].lower()
+        if "jquery" in src:
+            result["javascript"].append("jQuery")
+        elif "react" in src:
+            result["javascript"].append("React")
+        elif "vue" in src:
+            result["javascript"].append("Vue.js")
+        elif "angular" in src:
+            result["javascript"].append("Angular")
+
+    # Deduplicate lists
+    result["technologies"] = list(set(result["technologies"]))
+    result["frameworks"] = list(set(result["frameworks"]))
+    result["javascript"] = list(set(result["javascript"]))
 
     return result
